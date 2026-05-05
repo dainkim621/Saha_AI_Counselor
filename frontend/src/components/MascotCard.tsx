21e55b385e9a402524ef890c1f84b17d9b3d2357
Geunyeong-Kwon <geuny1472@naver.com>
2026-05-06 08:20:02 +0900

    사하구청 마스코트 고우니 추가

--- frontend/src/components/MascotCard.tsx
+++ frontend/src/components/MascotCard.tsx
@@ -1,9 +1,16 @@
+import gouni from "../assets/gouni.png";
+
 function MascotCard() {
   return (
     <div className="mascot-card">
-      <div className="mascot">🐟</div>
-      <h2>사하챗봇</h2>
-      <p>사하구 민원 정보를 쉽게 찾아드릴게요.</p>
+      <img
+        src={gouni}
+        alt="사하구 캐릭터 고우니"
+        className="mascot-image"
+      />
+
+      <h2>고우니 챗봇</h2>
+      <p>사하구 민원 정보를 쉽고 친절하게 안내해드릴게요.</p>
     </div>
   );
 }
